12044ce21ebae52b4350c0a690c9a27cd7439298
ZombieGoldfisch <collinheuser484@gmail.com>
2025-06-22 10:59:31 +0200

    Remove redundant visual CLI and integrate map generation

---
+++ main.py
@@ -0,0 +1,11 @@
+"""Application entry point that launches the command line interface.
+
+Routes are visualized automatically using :func:`save_route_map` from the
+``visualization_osmnx`` module.
+"""
+
+from cli import run_cli
+
+
+if __name__ == "__main__":
+    run_cli()
